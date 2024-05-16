76d0d5481f8bbdbd8d4735dce7dfbad17e4c555f
fakeITDevTeam <dev5544455@gmail.com>
2024-05-16 18:50:14 +0700

    shop: modified: add filter products by category

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -447,19 +447,32 @@
     <input type="hidden" name="order" id="order" value="{{$order}}" />
     <input type="hidden" name="brands" id="brands" value="{{$q_brands}}" />
     <input type="hidden" name="categories" id="categories" value="{{$q_categories}}" />
+    <input type="hidden" name="prange" id="prange" value="">
 </form>
 @endsection
 
 @push("scripts")
     <script>
-        $("#pagesize").on("change", function() {
-            $("#size").val($("#pagesize option:selected").val());
-            $("#frmFilter").submit();
-        });
+        $(function() {
+            $("#pagesize").on("change", function() {
+                $("#size").val($("#pagesize option:selected").val());
+                $("#frmFilter").submit();
+            });
 
-        $("#orderby").on("change", function() {
-            $("#order").val($("#orderby option:selected").val());
-            $("#frmFilter").submit();
+            $("#orderby").on("change", function() {
+                $("#order").val($("#orderby option:selected").val());
+                $("#frmFilter").submit();
+            });
+
+            var $range = $(".js-range-slider");
+            instance = $range.data("ionRangeSlider");
+            instance.update({ from:{{$from}}, to:{{$to}} });
+
+            $("#prange").on("change", function() {
+                setTimeout(() => {
+                    $("#frmFilter").submit();
+                }, 1000);
+            })
         });
 
         function filterProductsByBrand(brand) {
