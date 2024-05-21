4030937ec77777a0c58dc5228a831b47a22d658c
fakeITDevTeam <dev5544455@gmail.com>
2024-05-21 20:55:02 +0700

    controller: modified: add getWishlistedProducts function

--- app/Http/Controllers/WishlistController.php
+++ app/Http/Controllers/WishlistController.php
@@ -7,8 +7,15 @@ use Gloudemans\Shoppingcart\Facades\Cart as Cart;
 
 class WishlistController extends Controller
 {
+    public function getWishlistedProducts() {
+        $items = Cart::instance("wishlist")->content();
+
+        return view('wishlist', ['items'=>$items]);
+    }
+
     public function addProductToWishlist(Request $request) {
         Cart::instance("wishlist")->add($request->id, $request->name, 1, $request->price)->associate('App\Models\Product');
+        
         return response()->json(['status'=>200, 'message'=>'Success! Item successfully added to your wishlist.']);
     }
 }
